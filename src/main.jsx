405187d0762440bda0bf81fb3d135a7effb8637c
Joe Worthington <joe@Joes-MacBook-Pro.local>
2023-04-28 08:07:53 +0100

    changed structure of styles and hooks into assets

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.scss';
-import '../src/styles/_variables.scss';
+import '../src/assets/styles/_variables.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
